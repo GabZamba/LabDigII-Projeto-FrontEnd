a5d98b21fd1effe73321c7b28b4aed76547a7d71
Gabriel Zambelli <gabrielzsb.comp@usp.br>
2022-11-30 16:28:16 -0300

    🎨 Refactor code and styling
    Co-authored-by: jvtdegelo <jvtdegelo@users.noreply.github.com>
    Co-authored-by: Pedro de Santi <PedroDeSanti@users.noreply.github.com>

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,5 @@
 export const urlGet = 'https://labdigi-nodered.rj.r.appspot.com/data';
 
-export const urlPost = 'https://labdigi-nodered.rj.r.appspot.com/distance';
-export const urlPostGet = 'https://labdigi-nodered.rj.r.appspot.com/dist';
+export const urlSend = 'https://labdigi-nodered.rj.r.appspot.com/dist';
 
 export const intervalTimer = 1000;
