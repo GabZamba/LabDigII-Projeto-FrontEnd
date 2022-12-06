d9b1b74c14a68f1049120fef0dae10ecdd58f9fa
GabZamba <gabrielzsb.comp@usp.br>
2022-12-05 23:55:38 -0300

    🔥 Remove Drawing component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Drawing } from './Drawing';
 import { LineChart } from './LineChart';
 import { SliderElement } from './Slider';
 
 function App() {
     return (
         <div className="App">
-            {/* <Drawing></Drawing> */}
             <SliderElement></SliderElement>
             <LineChart></LineChart>
         </div>
